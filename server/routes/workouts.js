d6dbc0e927951319df32b455c807beaf60b83d0e
Gayatri Agarwal <gayatri19agarwal@gmail.com>
2024-01-17 23:10:20 +0530

    added workout controllers

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,31 +1,15 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
 const router = express.Router();
+const {createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout} = require('../controllers/workoutController')
 
-router.get("/", (req, res) => {
-  res.json({ mssg: "get all" });
-});
+router.get("/", getWorkouts)
 
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "get single" });
-});
+router.get("/:id", getWorkout)
 
-router.post("/", async (req, res) => {
-  const { title, reps, load } = req.body;
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout)
 
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "delete" });
-});
+router.delete("/:id", deleteWorkout)
 
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "update" });
-});
+router.patch("/:id", updateWorkout)
 
 module.exports = router;
