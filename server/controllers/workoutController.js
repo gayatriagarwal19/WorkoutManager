d2220e43f3ba4c6873bbbd56a3980ecd4af8114a
Gayatri Agarwal <gayatri19agarwal@gmail.com>
2024-01-21 16:30:19 +0530

    Handling Error Responses

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -25,6 +25,23 @@ const getWorkout = async (req, res) => {
 //create new workout
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+
+  if(emptyFields.length >0 ){
+    return res.status(400).json({error: 'please fill in all the fields', emptyFields})
+  }
+
   try {
     const workout = await Workout.create({ title, reps, load });
     res.status(200).json(workout);
